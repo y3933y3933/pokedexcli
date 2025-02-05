05b2b53e962a3932126783703539747cfecb3dc7
Joanne Lin <y3933y3933@gmail.com>
2025-02-05 21:45:33 +0800

    feat: add startRepl func

--- pokedexcli/repl.go
+++ pokedexcli/repl.go
@@ -1,8 +1,24 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		inputs := cleanInput(reader.Text())
+		fmt.Printf("Your command was: %s\n", inputs[0])
+	}
+
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
