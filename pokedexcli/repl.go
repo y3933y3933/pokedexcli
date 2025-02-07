94ee13b90ca21cbb18432ec373a8338381112467
Joanne Lin <y3933y3933@gmail.com>
2025-02-07 15:10:31 +0800

    feat: add exit and help commands

--- pokedexcli/repl.go
+++ pokedexcli/repl.go
@@ -15,7 +15,25 @@ func startRepl() {
 		reader.Scan()
 
 		inputs := cleanInput(reader.Text())
-		fmt.Printf("Your command was: %s\n", inputs[0])
+
+		if len(inputs) == 0 {
+			continue
+		}
+		commandName := inputs[0]
+
+		command, exists := getCommands()[commandName]
+
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+
 	}
 
 }
@@ -24,4 +42,25 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
